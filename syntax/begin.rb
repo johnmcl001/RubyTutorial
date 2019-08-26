80940ee8417c691abb3d788c5685a8bfc2ebf64b
johnmcl001 <john.mc-loughlin.3@ucdconnect.ie>
2019-08-26 14:41:19 +0100

    Ruby syntax done

---
+++ syntax/begin.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby -w
+#
+puts "This is the main Ruby program"
+
+END {
+	puts "Terminating Ruby program"
+}
+
+BEGIN {
+	puts "Initializing Ruby Program"
+}
---
+++ syntax/eof.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/ruby -w
+
+print <<EOF
+	This is the first way of creating
+	here document ie. multiple line string.
+EOF
+
+print <<"EOF";	# Same as above
+	This is the second way of creating
+	here document ie. multiple line string.
+EOF
+
+print <<`EOC`
+	echo hi there
+	echo lo there
+EOC
+
+print <<"foo", <<"bar"
+	I said foo
+foo
+	I said bar
+bar
